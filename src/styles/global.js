4ca8166116a09262ef9eee0cacc7a782eb857775
Danilo Leite <danilo.cebolino@gmail.com>
2022-07-17 15:24:51 -0300

    create login system localstorage db

---
+++ src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f2f5;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+`;
+
+export default GlobalStyle;
